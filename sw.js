ed5394925e1e92add3d08eea4d473d45f38337df
kdhakal-8848 <kdhakal8848@gmail.com>
2026-07-26 14:46:56 -0400

    Initial deployment from Antigravity

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'creators-den-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './index.css',
+  './app.js',
+  './config.js',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
